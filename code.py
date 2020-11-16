7a8438f108deb1d03c81e6e543bb9a46699af7fc
Shivanshu10 <d0tc0m1@protonmail.com>
2020-11-16 11:21:01 +0530

    defined functions for API access

--- code.py
+++ code.py
@@ -3,6 +3,7 @@
 # request the API server as per choice
 # show data to user
 # Find what is webhook
+# Convert country name to country code
 
 # Later
 # GUI
@@ -21,47 +22,70 @@ from requests import get
 # make a get request
 from json import loads
 # convert json string to python dicitionory
-from os import exit
+from sys import exit
 # exit program with an exit code
 
 def statusCountry(site):
+    # get country name whose data is required
     # takes API site as param
-    # returns python  dict reprsenting status of all country
+    # returns python  dict reprsenting status of country
     # request API using GET method
     # convert response string to python dict
     # if error occurs print error code and reason of error
-    resp=get(site)
+    country=input("Country Code: ")
+    resp=get(site+"/"+country)
     if (resp.status_code==200):
-        json_dict=loads(resp.content)
+        return(loads(resp.content))
     else:
         print("Error " + resp.status_code + ":" + resp.reason)
         exit(1)
 
 def statusCountryDate(site):
-    # takes API site as param
+    # get country name and date at which status is required
     # get date on which data is required
-    # returns python dict reprsenting status of all country
-    # on specific date
+    # takes API site as param
+    # returns python dict reprsenting status of country till given date
     # request API using GET method
     # convert response string to python dict
     # if error occurs print error code and reason of error
+    country=input("Country Code: ")
     date=input("Date (YYYY-MM-DD): ")
-    resp=get(site, data={'date':date})
+    resp=get(site+"/"+country, data={'date':date})
     if (resp.status_code==200):
-        json_dict=loads(resp.content)
+        return(loads(resp.content))
     else:
         print("error " + resp.status_code + ":" + resp.reason)
         exit(1)
 
-
-def diff(site):
-    pass
-
 def diffCountry(site):
-    pass
+    # get country name
+    # takes API site as param
+    # returns python dict reprsenting data
+    # request API using GET method
+    # convert response string to python dict
+    # if error occurs print error code and reason of error
+    country=input("Country Code: ")
+    resp=get(site+"/diff/"+country)
+    if (resp.status_code==200):
+        return(loads(resp.content))
+    else:
+        print("error " + resp.status_code + ":" + resp.reason)
+        exit(1)
 
 def predictionCountry(site):
-    pass
+    # get country name
+    # takes API site as param
+    # returns python dict reprsenting data
+    # request API using GET method
+    # convert response string to python dict
+    # if error occurs print error code and reason of error
+    country=input("Country Code: ")
+    resp=get(site+"/prediction/"+country)
+    if (resp.status_code==200):
+        return(loads(resp.content))
+    else:
+        print("error " + resp.status_code + ":" + resp.reason)
+        exit(1)   
 
 def menu(site):
     # pass site url as parameter
@@ -71,9 +95,8 @@ def menu(site):
         print("COVID19 Tracer with ML prediction")
         print("1. Get status by Country")
         print("2. Get status by country and date")
-        print("3. Difference between Latest state and previous one")
-        print("4. Difference between Latest state and previous one by country")
-        print("5. Get two weeks prediction by specific country")
+        print("3. Difference between Latest state and previous one by country")
+        print("4. Get two weeks prediction by specific country")
         print("99. Exit")
         choice = int(input("Choice> "))
 
@@ -82,10 +105,8 @@ def menu(site):
         elif (choice==2):
             statusCountryDate(site)
         elif (choice==3):
-            diff(site)
-        elif (choice==4):
             diffCountry(site)
-        elif (choice==5):
+        elif (choice==4):
             predictionCountry(site)
         elif (choice==99):
             exit(0)
