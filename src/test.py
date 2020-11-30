270c1d4d9953503e6f08780aa78e3b6a2e23fb3d
GouravShDev <Gourav0Sharma1@gmail.com>
2020-11-30 19:16:10 +0530

    Updated Gitignore and removed Test files

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from pynotifier import Notification
-
-notifyTitle="Test"
-notifyDescription="test"
-
-notice=Notification(title=notifyTitle, description=notifyDescription, duration=5, urgency=Notification.URGENCY_CRITICAL)
-notice.send()
